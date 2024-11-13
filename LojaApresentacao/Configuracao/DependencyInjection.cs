2d2198e495321386272a18e2690f8e1f156df132
Joao Guilhermem <joaoguilhermeoboista@gmail.com>
2024-11-13 13:25:30 -0300

    Criação da apresentação

---
+++ LojaApresentacao/Configuracao/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using AplicationCatalogo.Services;
+using CatalogoData;
+using CatalogoData.Repository;
+using CatelogoDominio;
+using CatelogoDominio.Interfaces;
+using ServicoCore.Bus;
+
+namespace LojaApresentacao.Configuracao
+{
+    public static class DependencyInjection
+    {
+
+        public static void RegisterServices(this IServiceCollection services) 
+        { 
+            //Domain Bus
+            services.AddScoped<IMediartHandler,MediartHandler>();
+
+            // Catalogo
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddScoped<IProdutoAppServices,ProdutoAppService>();
+            services.AddScoped<IEstoqueService, EstoqueService>();
+            services.AddScoped<CatalogoContexto>();
+        }
+    }
+}
---
+++ LojaApresentacao/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LojaApresentacao.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
